a0b7db09f0b2ec6c865b38199b1f3f58721c2f12
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-04-30 13:21:08 +0530

    border-panel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer"
+import Header from "./border-panel/Header";
+import Footer from "./border-panel/Footer"
 
 export default function App() {
     return (
