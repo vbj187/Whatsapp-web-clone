c6a1354afc859bbb7c6c1eb8209ede76ee85d87f
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-04-30 14:19:32 +0530

    console init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./border-panel/Header";
-import Footer from "./border-panel/Footer"
+import Footer from "./border-panel/Footer";
+import Console from "./console/Console"
 
 export default function App() {
     return (
         <div>
             <Header />
+            <Console />
             <Footer />
         </div>
     )
---
+++ src/components/console/Console.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Console() {
+    return (
+        <div>
+            <div className="column" style={{ backgroundColor: "#CAD3C8" }}>
+                <p>left panel</p>
+            </div>
+            <div className="column" style={{ backgroundColor: "#182C61" }}>
+                <p>right panel</p>
+            </div>
+        </div>
+    )
+}
