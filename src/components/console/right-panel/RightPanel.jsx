626707c64bb52d51cf5fb1854dd76a42b705c83c
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-01 19:57:08 +0530

    almost done with chatbox area

--- src/components/console/right-panel/RightPanel.jsx
+++ src/components/console/right-panel/RightPanel.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ChatRecipientBar from "./components/ChatRecipientBar"
+import ChatBox from "./components/ChatBox";
 
 export default function RightPanel() {
     return (
         <div className="right-panel">
             <ChatRecipientBar />
+            <ChatBox />
         </div>
     )
 }
--- src/components/console/right-panel/components/ChatBox.jsx
+++ src/components/console/right-panel/components/ChatBox.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import SendIcon from '@material-ui/icons/Send';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
+import { red } from "@material-ui/core/colors";
 
 export default function ChatBox() {
     return (
-        <div>
-            <InsertEmoticonIcon />
-            <AttachFileIcon />
-            <SendIcon />
+        <div className="chat-box-panel">
+            <button><InsertEmoticonIcon /></button>
+            <div className="chat-textarea-encloser">
+                <input
+                    className="chat-textArea"
+                    type="text"
+                    placeholder="Type your message"
+                    id="chat-input-textarea"
+                />
+            </div>
+            <div className="mic-send-encloser">
+                <button><MicIcon /></button>
+                <button><SendIcon /></button>
+            </div>
         </div>
     )
 }
