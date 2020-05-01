6131e0c78dc67dc7b7df9c8f65cc746822759565
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-01 23:33:55 +0530

    left panel 3 divs

--- src/components/console/left-panel/LeftPanel.jsx
+++ src/components/console/left-panel/LeftPanel.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import UserPane from "./components/UserPane";
 import SearchChat from './components/SearchChat';
+import ChatContacts from "./components/ChatContacts";
 
 export default function LeftPanel() {
     return (
         <div className="left-panel">
             <UserPane />
             <SearchChat />
+            <ChatContacts />
         </div>
     )
 }
---
+++ src/components/console/left-panel/components/ChatContacts.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ChatContacts() {
+    return (
+        <div className="chat-contacts">
+
+        </div>
+    )
+}
