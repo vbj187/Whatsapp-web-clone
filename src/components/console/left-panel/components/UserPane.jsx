f54621c3492eb071c49969970ece93349e3f9428
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-01 14:36:21 +0530

    console height fixed 'vh'

--- src/components/console/left-panel/components/UserPane.jsx
+++ src/components/console/left-panel/components/UserPane.jsx
@@ -3,12 +3,14 @@ import ProfilePicture from "../../../mini-components/ProfilePicture";
 
 import NewChatIcon from '@material-ui/icons/InsertComment';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Avatar from '@material-ui/core/Avatar';
+
 
 export default function UserPane() {
     return (
         <div className="userPane">
             <div className="nameAndPic">
-                <ProfilePicture />
+                <Avatar alt="Profile Picture" src="https://images.pexels.com/photos/399772/pexels-photo-399772.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
                 <h3 id="defaultUserName">Vigneshwaran</h3>
             </div>
             <div className="left-panel-optioins">
